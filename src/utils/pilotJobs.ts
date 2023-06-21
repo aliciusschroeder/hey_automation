b180e45af575272dbaf71e9ab9124d772f99d13c
Alicius Schröder <alicius.schroeder@gmail.com>
2023-06-21 19:32:25 +0200

    Initial commit

---
+++ src/utils/pilotJobs.ts
@@ -0,0 +1,9 @@
+// ./src/utils/pilotJobs.ts
+
+export const jobsInPilotAllowed = [
+    { jobtype: 'Engineer', pilot_allowed: true },
+    { jobtype: 'Designer', pilot_allowed: false },
+    { jobtype: 'Handwerker/-in', pilot_allowed: true },
+    { jobtype: 'Manager', pilot_allowed: true },
+  ];
+  
